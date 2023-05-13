2b4f1a6599a44ef00b3f528d7285f794b27b782a
luisguilheerme <lg.maroni@gmail.com>
2023-05-13 19:53:34 -0300

    Custom query and projection

--- src/main/java/com/luisguilherme/dslist/dto/GameMinDTO.java
+++ src/main/java/com/luisguilherme/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.luisguilherme.dslist.dto;
 
 import com.luisguilherme.dslist.entities.Game;
+import com.luisguilherme.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	
@@ -22,6 +23,14 @@ public class GameMinDTO {
 		shortDescription = entity.getShortDescription();
 	}
 
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
+	}
+	
 	public Long getId() {
 		return id;
 	}
--- src/main/java/com/luisguilherme/dslist/services/GameService.java
+++ src/main/java/com/luisguilherme/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.luisguilherme.dslist.dto.GameDTO;
 import com.luisguilherme.dslist.dto.GameMinDTO;
 import com.luisguilherme.dslist.entities.Game;
+import com.luisguilherme.dslist.projections.GameMinProjection;
 import com.luisguilherme.dslist.repositories.GameRepository;
 
 @Service
@@ -27,5 +28,11 @@ public class GameService {
 	public GameDTO findbyID(Long id) {
 		Game result = gameRepository.findById(id).get();
 		return new GameDTO(result);		
+	}
+	
+	@Transactional(readOnly=true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
